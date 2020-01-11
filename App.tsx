05f28ba1a6f796eca808daa0900b477e3c48825e
hyun <coderhyun476@gmail.com>
2020-01-11 16:46:03 +0900

    vector-icon 연결

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import configureStore from './src/modules';
+// import configureStore from './src/modules';
 import AppContainer from './src/screens';
 
-const { store } = configureStore();
+// const { store } = configureStore();
 
 const App = () => {
   return (
     <>
-      <Provider store={store}>
-        {/* <PersistGate loading={null} persistor={persistor}> */}
-        <AppContainer />
-        {/* </PersistGate> */}
-      </Provider>
+      {/* <Provider store={store}> */}
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <AppContainer />
+      {/* </PersistGate> */}
+      {/* </Provider> */}
     </>
   );
 };
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/AntDesign'
 
 const HomeScreen = () => {
     return (
         <View>
             <Text>Home</Text>
+            <Icon name="heart" size={30} color="#900" />
         </View>
     )
 }
