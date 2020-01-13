bafb7a031bd270225360f522052dd8c43806b578
hyun <coderhyun476@gmail.com>
2020-01-13 23:48:57 +0900

    Code-push 연동

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-// import configureStore from './src/modules';
+import configureStore from './src/modules';
 import AppContainer from './src/screens';
+import codePush, { CodePushOptions } from "react-native-code-push";
 
-// const { store } = configureStore();
+const { store, persistor } = configureStore();
+
+let codePushOptions: CodePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
 
 const App = () => {
   return (
     <>
-      {/* <Provider store={store}> */}
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <AppContainer />
-      {/* </PersistGate> */}
-      {/* </Provider> */}
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <AppContainer />
+        </PersistGate>
+      </Provider>
     </>
   );
 };
 
 
-export default App;
+export default codePush(codePushOptions)(App);
--- android/app/src/main/java/com/saladfactory/thundermart/MainApplication.java
+++ android/app/src/main/java/com/saladfactory/thundermart/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
+        }
+
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
         }
 
         @Override
